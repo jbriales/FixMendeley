efd2ffe3c86d22e83d220cfac76c8509788a8521
Jesus Briales <jesusbriales@gmail.com>
2018-09-24 21:48:21 +0200

    Create venues.json and load

--- fixmendeley/fix_venues.py
+++ fixmendeley/fix_venues.py
@@ -1,5 +1,12 @@
 # coding=utf-8
 
+from pkg_resources import resource_filename
+from termcolor import colored
+
+import json
+from peewee import SqliteDatabase
+from fixmendeley.my_mendeley_models import *
+
 
 def fix_venues():
     """
@@ -7,4 +14,16 @@ def fix_venues():
     :rtype: object
     """
     print("Fixing publication field")
+
+    # Load our database of venue names and keywords
+    with open(resource_filename('fixmendeley', 'venues.json'), 'r') as f:
+        venues = json.load(f)
+
+    print(venues)
+
+    # For each conference/journal registered in our dictionary
+
+    # Query for rows using ordered keywords
+
+    # Set field with normalized value for all fields
     pass
